ee1b97bdc724e9b39736ef71e9a919ee30c8efc6
박현아 <hyuna-park@baghyeon-aui-MacBookPro.local>
2024-01-27 17:33:07 +0900

    init: project setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const path = require('path')
+import * as path from 'path';
+
+const __dirname = path.resolve();
+
+const nextConfig = {
+
+    sassOptions: {
+        includePaths: [path.join(__dirname, '.')],
+    },
+
+
+};
 
 export default nextConfig;
